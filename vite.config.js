fa3fc86f8b93359de41a2ecb57f65298afccc122
Oleksandr <sash1983direct@gmail.com>
2025-07-07 19:54:15 -0700

    Dz 2 Modul 9

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/goit-js-hw-09/',
     root: 'src',  // корень проекта — папка src
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
