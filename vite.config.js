37f8d0efc0ae4e892f643f54f33c1654d1f7b776
Oleksandr <sash1983direct@gmail.com>
2025-07-13 19:31:49 -0700

    DZ9 Modul 9

--- vite.config.js
+++ vite.config.js
@@ -4,50 +4,42 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
-export default defineConfig(({ command }) => {
-  return {
-    base: '/goit-js-hw-09/',
-    root: 'src',  // корень проекта — папка src
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    build: {
-      sourcemap: true,
-      outDir: '../dist',
-      emptyOutDir: true,
-      rollupOptions: {
-        // Входные точки — все html из src
-        input: glob.sync('./src/*.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames(chunkInfo) {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
-          assetFileNames(assetInfo) {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
+export default defineConfig({
+  base: '/goit-js-hw-09/',
+  root: 'src',
+  build: {
+    sourcemap: true,
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: glob.sync('./src/*.html'),
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+        entryFileNames(chunkInfo) {
+          return '[name].js';
+        },
+        assetFileNames(assetInfo) {
+          if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+            return '[name].[ext]';
+          }
+          return 'assets/[name]-[hash][extname]';
         },
       },
     },
-    plugins: [
-      injectHTML(),
-      FullReload(['./src/**/*.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
-    ],
-    optimizeDeps: {
-      include: ['simplelightbox'],  // обязательно для корректной работы импорта
-    },
-  };
+  },
+  plugins: [
+    injectHTML(),
+    FullReload(['./src/**/*.html']),
+    SortCss({
+      sort: 'mobile-first',
+    }),
+  ],
+  optimizeDeps: {
+    include: ['simplelightbox'],
+  },
 });
+
